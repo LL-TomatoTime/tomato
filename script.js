23623a7fcbdd3ead2b3f426e3dcee00d1e9eb0af
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-07-04 20:41:19 -0300

    I started with Js, creating the variables for the continuation of the project, There many things to change and improve in my code yet, but I'm trying

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ rail.addEventListener('click', ()=>{
     rail.classList.toggle('dark')
     body.classList.toggle('dark')
 });
+
+let timer;
+let timeLeft = 25 * 60;
+let isRunning = false;
+let isBreak = false;
+let pomodoroDuration = 25;
+let breakDuration = 25;
+let cycles = 0;
+
+const timerDisplay = document.getElementById('timer');
+const startBtn = document.getElementById('start');
+const pauseBtn = document.getElementById('pause');
+const resetBtn = document.getElementById('reset');
