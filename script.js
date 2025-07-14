cb37fc26c5fa86ca73e4f77d23859984537036f0
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-07-13 22:32:01 -0300

    I made the start button work, making it start the pomodoro count, going from 25 to 0. I used the setInterval command to do this. The comments below code are just draft for the next steps.

--- script.js
+++ script.js
@@ -6,15 +6,43 @@ rail.addEventListener('click', ()=>{
     body.classList.toggle('dark')
 });
 
-let timer;
-let timeLeft = 25 * 60;
-let isRunning = false;
-let isBreak = false;
-let pomodoroDuration = 25;
-let breakDuration = 25;
-let cycles = 0;
+let timerDisplay = document.getElementById('timer'); //DOM for display
+let startBnt = document.getElementById('start'); //DOM for start Button
 
-const timerDisplay = document.getElementById('timer');
-const startBtn = document.getElementById('start');
-const pauseBtn = document.getElementById('pause');
-const resetBtn = document.getElementById('reset');
+let timeLeft = 25 * 60; //convert 25 into seconds, for the counter decrease second per second on the display
+
+function startTimer() {
+    const timeSetInterval = setInterval(() => {
+
+        if(timeLeft <= 0){
+            clearInterval(timeSetInterval);  //stop the setInterval
+            alert('The time is up!!')
+            return
+        }
+
+        timeLeft--;
+        const minutes = Math.floor(timeLeft / 60);  //convert seconds into minutes, for decrease the minutes on the display
+        const seconds = timeLeft % 60 //honestly I didn't understand this command, why would I have to use '%' (I know what is '%', and how it works)
+        timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`; //Just show it on the display
+    }, 1000); //I don't know why I would have to use milliseconds instead of just seconds, but I saw this in the w3school documentation, please explain this to me
+}
+
+startBnt.addEventListener('click', startTimer);
+
+
+
+// let pauseBtn = document.getElementById('pause');
+
+// function pauseTimer() {
+//     if (pauseBtn.addEventListener('click', pauseTimer)){
+//         clearInterval(timerInterval);
+//     }
+// }
+
+// let resetBtn = document.getElementById('reset');
+
+// function resetTimer() {
+//     timerDisplay.startTimer = '25:00'
+// }
+
+// reset.addEventListener('click', resetTimer);
