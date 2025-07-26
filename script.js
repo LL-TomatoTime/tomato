656ae2a923f1222ef2084f413189527aa9e6786c
Azherenn <magno.ferreira2408@gmail.com>
2025-07-26 18:32:54 -0300

    Minor changes to SetInterval

--- script.js
+++ script.js
@@ -9,35 +9,25 @@ rail.addEventListener('click', ()=>{
 let timerDisplay = document.getElementById('timer'); //DOM for display
 let startBnt = document.getElementById('start'); //DOM for start Button
 
-let timeLeft = 1 * 6; //convert 25 into seconds, for the counter decrease second per second on the display
-
+let timeLeft = 25 * 60; //convert 25 into seconds, for the counter decrease second per second on the display
 let timerInterval = null;
 
 function startTimer() {
 
-    if (timerInterval !== null) {
-        return;
-    }
+    if (timerInterval !== null) return;
 
     timerInterval = setInterval (() => {
-        if(timeLeft <= 0){
-            clearInterval;
-            timerInterval = null;
+        if(timeLeft <= 0){ //Check if the timer has reached 0
+            clearInterval(timerInterval); //Clears SetInterval ID
+            timerInterval = null; // Leave it as null so it can be used again
+            alert('The time is up!!');
             return;
-        }
-    })
-    const timeSetInterval = setInterval(() => {
-
-        if(timeLeft <= 0){
-            clearInterval(timeSetInterval);  //stop the setInterval
-            alert('The time is up!!')
-            return
         }
 
         timeLeft--;
         const minutes = Math.floor(timeLeft / 60);  //convert seconds into minutes, for decrease the minutes on the display
         const seconds = timeLeft % 60 //honestly I didn't understand this command, why would I have to use '%' (I know what is '%', and how it works)
-        timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`; //Just show it on the display
+        timerDisplay.textContent = `${minutes}:${String(seconds).padStart(2, '0')}`; //Just show it on the display
     }, 1000); //I don't know why I would have to use milliseconds instead of just seconds, but I saw this in the w3school documentation, please explain this to me
 }
 
