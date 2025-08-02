3f1724b070b65adcd92fb1e3b59c1ebdb79ec86c
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-08-02 19:20:25 -0300

    I did the pause and reset function. It was simple, i create another 2 functions, and I used the start code function as a base to do this. I also create the function updateDisplay for literally update the display when i reset the time, and I use this as the default setting for our timer.

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ rail.addEventListener('click', ()=>{
 
 let timerDisplay = document.getElementById('timer'); //DOM for display
 let startBnt = document.getElementById('start'); //DOM for start Button
+let pauseBtn = document.querySelector('#pause');
+let resetBtn = document.querySelector('#reset');
 const alarmSound = document.getElementById('alarm-sound'); //DOM for alarm
 
-let timeLeft = 25 * 60; //convert 25 into seconds, for the counter decrease second per second on the display
+let originalTime = 25 * 60;//convert 25 into seconds, for the counter decrease second per second on the display
+let timeLeft = originalTime;
 let timerInterval = null;
+
+function updateDisplay() {
+    const minutes = Math.floor(timeLeft / 60);  //convert seconds into minutes, for decrease the minutes on the display
+    const seconds = timeLeft % 60 //honestly I didn't understand this command, why would I have to use '%' (I know what is '%', and how it works)
+    timerDisplay.textContent = `${minutes}:${String(seconds).padStart(2, '0')}`; //Just show it on the display
+}
 
 function startTimer() {
 
@@ -21,34 +30,31 @@ function startTimer() {
         if(timeLeft <= 0){ //Check if the timer has reached 0
             clearInterval(timerInterval); //Clears SetInterval ID
             timerInterval = null; // Leave it as null so it can be used again
-            alert('The time is up!!');
             alarmSound.play();
+            alert('The time is up!!');
             return;
         }
 
         timeLeft--;
-        const minutes = Math.floor(timeLeft / 60);  //convert seconds into minutes, for decrease the minutes on the display
-        const seconds = timeLeft % 60 //honestly I didn't understand this command, why would I have to use '%' (I know what is '%', and how it works)
-        timerDisplay.textContent = `${minutes}:${String(seconds).padStart(2, '0')}`; //Just show it on the display
+       updateDisplay();
     }, 1000); //I don't know why I would have to use milliseconds instead of just seconds, but I saw this in the w3school documentation, please explain this to me
 }
 
+function pauseTimer(){
+    if(timerInterval !== null){ //if the variable timerInterval is different from null, that is, running the time
+        clearInterval(timerInterval); // this command will stop the timer
+        timerInterval = null; //and this command will return the timer to default
+    }
+}
+
+function resetTimer(){
+    pauseTimer();
+    timeLeft = originalTime;
+    updateDisplay();
+}
+
 startBnt.addEventListener('click', startTimer);
+pauseBtn.addEventListener('click', pauseTimer);
+resetBtn.addEventListener('click', resetTimer);
 
 
-
-// let pauseBtn = document.getElementById('pause');
-
-// function pauseTimer() {
-//     if (pauseBtn.addEventListener('click', pauseTimer)){
-//         clearInterval(timerInterval);
-//     }
-// }
-
-// let resetBtn = document.getElementById('reset');
-
-// function resetTimer() {
-//     timerDisplay.startTimer = '25:00'
-// }
-
-// reset.addEventListener('click', resetTimer);
